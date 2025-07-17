2bb0a7c2673a81dd27206b5930fc81b20bb3bef3
Endris <endrissuofe@gmail.com>
2025-07-17 09:10:54 +0100

    Add unit and integration test, update CI workflow with linting

--- test.js
+++ test.js
@@ -1,6 +1,26 @@
-const assert = require('assert');
-describe('Basic Test', () => {
-  it('should return true', () => {
-    assert.equal(true, true);
+const { expect } = require('chai');
+const request = require('supertest');
+const app = require('./index');
+
+// Unit Test Example
+describe('Unit Tests', () => {
+  it('should return true for a simple check', () => {
+    const simpleCheck = () => true;
+    expect(simpleCheck()).to.be.true;
+  });
+});
+
+// Integration Test Example
+describe('Integration Tests - Express App', () => {
+  it('should return 200 and correct response body for GET /', (done) => {
+    request(app)
+      .get('/')
+      .expect(200)
+      .expect('Content-Type', /html/)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.text).to.include('<h1>Welcome to My CI/CD Demo</h1>');
+        done();
+      });
   });
 });
